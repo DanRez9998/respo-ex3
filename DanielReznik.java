dded9a522c32ea1234142d6acdeaaa75fa69c8d2
DanRez9998 <danielreznik67@gmail.com>
2022-11-13 19:29:02 +0200

    fourth change  commit two f in one time

--- DanielReznik.java
+++ DanielReznik.java
@@ -1,7 +1,7 @@
 public class DanielReznik{
 	public static void main(String[]args){
 		for(int i=1;i<=10;i++){
-			System.out.println("Hello World my name is Daniel Reznik..."+i);
+			System.out.println("Hello World my name is Daniel Reznik.C1.."+i);
 			}
 		}
 	}
